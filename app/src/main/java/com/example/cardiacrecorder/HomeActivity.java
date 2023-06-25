480e6146b7336e47ba33dc727dcb1ebbf104514b
Nushrat-Tarmin-Meem <meem.cse.2k19@gmail.com>
2023-06-25 20:25:02 +0600

    Add & View Record UI Done!

--- app/src/main/java/com/example/cardiacrecorder/HomeActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/HomeActivity.java
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
-    private Button btnlogOut;
+    private Button btnlogOut,AddBtn, ViewBtn;
     private FirebaseAuth mAuth;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -19,11 +20,29 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btnlogOut=findViewById(R.id.blo);
+        AddBtn=findViewById(R.id.addRecord);
+        ViewBtn=findViewById(R.id.viewRecord);
         mAuth=FirebaseAuth.getInstance();
         btnlogOut.setOnClickListener(view -> {
             mAuth.signOut();
             startActivity(new Intent(HomeActivity.this,MainActivity.class));
         });
+        AddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, AddRecordActivity.class);
+                startActivity(intent);
+            }
+        });
+        ViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ViewRecordActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
     @Override
     protected void onStart() {
